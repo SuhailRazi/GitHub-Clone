ec2b2eaae31931f63b7d34695a75505addfeb7fb
suhail <suhail.r@hubspire.com>
2024-03-02 22:35:16 +0530

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+import SideBar from "./components/SideBar";
 function App() {
   return (
-    <div className="text-red-500 bg-blue-400 p-2">Hello Form home page</div>
+    <div className="flex text-white">
+      <SideBar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+        <footer>Footer</footer>
+      </div>
+    </div>
   );
 }
 
