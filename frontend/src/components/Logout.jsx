75fbed0e8507738ba271662ea2aec14a0da35517
suhail <suhail.r@hubspire.com>
2024-03-24 15:41:27 +0530

    Authentication and logout, login implemented in signuppage, login page, sidebar functionality

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,13 +1,32 @@
 import { MdLogout } from "react-icons/md";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const Logout = () => {
+  const { authUser, setAuthUser } = useAuthContext();
+  const handleLogout = async () => {
+    try {
+      const res = await fetch("/api/auth/logout", {
+        credentials: "include",
+      });
+      const data = await res.json("");
+      toast.success(data.message);
+      setAuthUser(null);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <>
       <img
-        src="./java.svg"
+        src={authUser?.avatarUrl}
         className="w-10 h-10 rounded-full border border-gray-800"
       />
-      <div className="cursor-pointer flex items-center p-2 rounded-lg bg-glass mt-auto">
+      <div
+        className="cursor-pointer flex items-center p-2 rounded-lg bg-glass mt-auto"
+        onClick={handleLogout}
+      >
         <MdLogout size={22} />
       </div>
     </>
