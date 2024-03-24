2d9453e2be27ae5685cc56ba4f6f9cc822e6d565
suhail <suhail.r@hubspire.com>
2024-03-24 21:23:44 +0530

    Optimsed build for production

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectMongoDb from "./db/connectMongoDb.js";
 import passport from "passport";
 import session from "express-session";
+import path from "path";
 
 import "./passport/github.auth.js";
 
@@ -11,7 +12,12 @@ import userRoutes from "./Routes/user.route.js";
 import explorerRoutes from "./Routes/explorer.route.js";
 import authRoutes from "./Routes/auth.route.js";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+// Deployement process where the backedn and frontend run on same domain
+const __dirname = path.resolve();
+console.log("dirname", __dirname);
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -22,17 +28,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(cors());
-dotenv.config();
-
-app.get("/", (req, res) => {
-  res.send("Welcome to rest API");
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explorer", explorerRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on 5000");
+// Static routes for frontend
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
   connectMongoDb();
 });
