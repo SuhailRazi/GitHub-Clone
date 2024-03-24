12ef4ae16d95cba4a26f4c480a62422c3ec93ae8
suhail <suhail.r@hubspire.com>
2024-03-24 17:30:10 +0530

    removed local host from login with github function

--- frontend/src/lib/functions.js
+++ frontend/src/lib/functions.js
@@ -1,3 +1,3 @@
 export const handleLoginWithGithub = () => {
-  window.open("http://localhost:5000/api/auth/github", "_self"); //Redirect to GithubLogin page
+  window.open("/api/auth/github", "_self"); //Redirect to GithubLogin page
 };
