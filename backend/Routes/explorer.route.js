737fce06b8cbfd4fae2df684c7282ec5884cddc8
suhail <suhail.r@hubspire.com>
2024-03-24 17:27:13 +0530

    Authenticator middleware implemented in likes get api

--- backend/Routes/explorer.route.js
+++ backend/Routes/explorer.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { explorePopularRepos } from "../controllers/explorer.controller.js";
+import ensureAuthenticated from "../middleware/ensureAuthenticated.js";
 
 const router = express.Router();
 
--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getLikes,
   getUserProfileInfo,
   likeProfile,
 } from "../controllers/user.controller.js";
@@ -8,8 +9,8 @@ import ensureAuthenticated from "../middleware/ensureAuthenticated.js";
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfileInfo);
-// TODO -> likes, like a profile (get, Post)
 
+router.get("/likes", ensureAuthenticated, getLikes);
 router.post("/like/:username", ensureAuthenticated, likeProfile);
 
 export default router;
