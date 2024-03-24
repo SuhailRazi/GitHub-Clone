37cf9f3bf740cfaeaf3d30b8aad9187bbd252ad6
suhail <suhail.r@hubspire.com>
2024-03-24 16:21:14 +0530

    Post request for liking the profile

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getUserProfileInfo } from "../controllers/user.controller.js";
+import {
+  getUserProfileInfo,
+  likeProfile,
+} from "../controllers/user.controller.js";
+import ensureAuthenticated from "../middleware/ensureAuthenticated.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfileInfo);
 // TODO -> likes, like a profile (get, Post)
+
+router.post("/like/:username", ensureAuthenticated, likeProfile);
 
 export default router;
