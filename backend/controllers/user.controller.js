9b8949d40efcef9d3523cb87c8f8c949feed774d
suhail <suhail.r@hubspire.com>
2024-03-24 16:20:38 +0530

    Controller login for liking the profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js";
+
 export const getUserProfileInfo = async (req, res) => {
   const { username } = req.params;
   try {
@@ -12,6 +14,38 @@ export const getUserProfileInfo = async (req, res) => {
     const repos = await repoRes.json();
 
     res.status(200).json({ userProf, repos });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const likeProfile = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findById(req.user._id.toString());
+    const userToLike = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (user.likedProfiles.includes(userToLike.username)) {
+      return res.status(400).json({ error: "User already liked" });
+    }
+
+    userToLike.likedBy.push({
+      username: user.username,
+      avatarUrl: user.avatarUrl,
+      likedDate: Date.now(),
+    });
+
+    user.likedProfiles.push(userToLike.username);
+
+    // await userToLike.save();
+    // await user.save();
+
+    await Promise.all([userToLike.save(), user.save()]);
+    res.status(200).json({ message: "User liked" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
